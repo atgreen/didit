1af15919570c3f8755b154cf55c859f6ce945faa
Anthony Green <green@moxielogic.com>
2021-08-08 08:06:45 -0400

    Add Tower support

--- src/alert.lisp
+++ src/alert.lisp
@@ -20,3 +20,24 @@
                            :content (json:encode-json-to-string
                                      `((:TEXT . ,(str:concat "ALERT: " message)))))
     (log:info a)))
+
+(defclass alert/tower (alert)
+  ((host :reader host)
+   (username :reader username)
+   (password :reader password)
+   (job-template :reader job-template)))
+
+(defmethod initialize-instance :after ((alert alert/tower) &key)
+  (with-slots (host username password job-template) alert
+    (setf host (gethash "host" config))
+    (setf username (gethash "username" config))
+    (setf password (gethash "password" config))
+    (setf job-template (gethash "job-template" config))))
+
+(defmethod send-alert ((alert alert/tower) message)
+  (log:info "alert/tower: ~A" message)
+  (with-slots (host username password job-template) alert
+    (let ((command (format nil "tower-cli --no-input -h ~A -u ~A -p ~A -J ~A"
+                           host username password job-template)))
+      (dolist (line (inferior-shell:run command))
+        (log:info line)))))

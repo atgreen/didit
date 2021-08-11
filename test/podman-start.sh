6adf9ed5d07a29a0fb6541225fd1fdfb8e62d4bd
Anthony Green <green@moxielogic.com>
2021-08-10 22:57:21 -0400

    3 node test. works-for-me.

--- test/podman-start.sh
+++ test/podman-start.sh
@@ -5,14 +5,20 @@ set -e
 podman pod stop didit-pod || true
 podman pod rm didit-pod || true
 
-BASE_HASH=$(sha512sum src/didit.asd | cut -b 1-8)
-podman images | grep ${BASE_HASH} > /dev/null 2>&1 || podman build -f build/Dockerfile.base --no-cache -t didit-base:${BASE_HASH} .
+BASE_HASH=$(sha512sum src/didit.asd | cut -b 1-8)1
+podman images | grep ${BASE_HASH} > /dev/null 2>&1 || podman build -f build/Dockerfile.base --no-cache -t didit-base:${BASE_HASH}1 .
 
-podman build -f build/Dockerfile -t didit:latest --no-cache --from didit-base:${BASE_HASH} src
+podman build -f build/Dockerfile -t didit:latest --no-cache --from didit-base:${BASE_HASH}1 src
 
-cat test/test-pod.yml | sed -e 's/\/home\/runner\/work\/didit\/didit/./' | podman play kube -
+#cat test/test-pod1.yml | sed -e 's/\/home\/runner\/work\/didit\/didit\/test\/config.ini/\/home\/green\/git\/didit\/test\/private1.ini/' | podman play kube -
+#cat test/test-pod2.yml | sed -e 's/\/home\/runner\/work\/didit\/didit\/test\/config.ini/\/home\/green\/git\/didit\/test\/private2.ini/' | podman play kube -
+#cat test/test-pod3.yml | sed -e 's/\/home\/runner\/work\/didit\/didit\/test\/config.ini/\/home\/green\/git\/didit\/test\/private3.ini/' | podman play kube -
 
-timeout -s TERM 45 bash -c 'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://localhost:8080/health)" != "200" ]]; do echo "Waiting for http://localhost:8080/health" && sleep 2; done'
+podman play kube test/test3-pod.yml
+
+timeout -s TERM 20 bash -c 'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://localhost:8080/health)" != "200" ]]; do echo "Waiting for http://localhost:8080/health" && sleep 2; done'
+#timeout -s TERM 20 bash -c 'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://localhost:8180/health)" != "200" ]]; do echo "Waiting for http://localhost:8080/health" && sleep 2; done'
+#timeout -s TERM 20 bash -c 'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://localhost:8280/health)" != "200" ]]; do echo "Waiting for http://localhost:8080/health" && sleep 2; done'
 
 echo ===========================================================================
 echo
@@ -26,4 +32,4 @@ echo Run \'make podman-stop\' to stop the test pod
 echo
 echo ===========================================================================
 
-podman logs -f didit-pod-didit
+podman logs -f didit-pod-didit1
